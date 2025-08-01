fd18c6f14a7fdfe3e53333898f7b539df932483a
FaresGawwad <faresellbbad@gmail.com>
2025-08-01 03:32:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,18 @@ if (localStorage.getItem('myProducts') != null) {
 } else {
     productsContainer = [];
 }
+
+
+
 function addProduct() {
+    if (productNameInput.value.trim() === "" ||
+        productPriceInput.value.trim() === "" ||
+        productCategoryInput.value.trim() === "" ||
+        productDescriptionInput.value.trim() === "") {
+
+        alert("Please fill in all fields before adding a product!");
+        return; 
+    }
     var product = {
         name: productNameInput.value,
         price: productPriceInput.value,
@@ -25,6 +36,7 @@ function addProduct() {
     localStorage.setItem('myProducts', JSON.stringify(productsContainer));
     clearForm();
     displayProducts(productsContainer);
+    window.alert('Your Product has been added.');
 }
 
 function clearForm() {
@@ -34,23 +46,23 @@ function clearForm() {
     productDescriptionInput.value = "";
 }
 
-function displayProducts (productList) {
+function displayProducts(productList) {
     var content = ``;
-    for(var i = 0 ; i < productList.length; i++) {
-        content += `<tr class="mx-2">
+    for (var i = 0; i < productList.length; i++) {
+        content += `<tr class="table-custom mx-2">
                 <td>${i}</td>
                 <td>${productList[i].name}</td>
                 <td>${productList[i].price}</td>
                 <td>${productList[i].category}</td>
                 <td>${productList[i].description}</td>
-                <td><button onclick="setFormForUpdate(${i})" class="btn btn-warning btn-sm my-2">update</button></td>
-                <td><button onclick="deleteProduct(${i})" class="btn btn-danger btn-sm my-2">delete</button></td>
+                <td><button onclick="setFormForUpdate(${i})" class="custom-bt btn-sm my-2 mx-auto">update</button></td>
+                <td><button onclick="deleteProduct(${i})" class="custom-bt btn-sm my-2 mx-auto">delete</button></td>
             </tr>`
     }
     document.getElementById('tableBody').innerHTML = content;
 }
 
-function searchProduct (searchTerm) {
+function searchProduct(searchTerm) {
     var serachResult = [];
     for (var i = 0; i < productsContainer.length; i++) {
         if (productsContainer[i].name.toLowerCase().includes(searchTerm.toLowerCase()) == true) {
@@ -60,42 +72,51 @@ function searchProduct (searchTerm) {
     displayProducts(serachResult);
 }
 
-function deleteProduct (deletedIndex) {
-    productsContainer.splice(deletedIndex,1);
+function deleteProduct(deletedIndex) {
+    productsContainer.splice(deletedIndex, 1);
     localStorage.setItem('myProducts', JSON.stringify(productsContainer));
     displayProducts(productsContainer);
-
+    window.alert('You deleted your product.');
 }
 
-function setFormForUpdate (updatedIndex) {
+function setFormForUpdate(updatedIndex) {
     productNameInput.value = productsContainer[updatedIndex].name;
     productPriceInput.value = productsContainer[updatedIndex].price;
     productCategoryInput.value = productsContainer[updatedIndex].category;
     productDescriptionInput.value = productsContainer[updatedIndex].description;
 
-    updateButton.classList.replace('d-none','d-inline-block');
+    updateButton.classList.replace('d-none', 'd-inline-block');
     addButton.classList.add('d-none');
     currentUpdateIndex = updatedIndex;
 }
 
 function updateProduct() {
+
     if (currentUpdateIndex !== -1) {
+        if (productNameInput.value.trim() === "" ||
+            productPriceInput.value.trim() === "" ||
+            productCategoryInput.value.trim() === "" ||
+            productDescriptionInput.value.trim() === "") {
+
+            alert("Please fill in all fields before updating the product!");
+            return; 
+        }
         var updatedProduct = {
             name: productNameInput.value,
             price: productPriceInput.value,
             category: productCategoryInput.value,
             description: productDescriptionInput.value
         };
-        
+
         productsContainer[currentUpdateIndex] = updatedProduct;
         localStorage.setItem('myProducts', JSON.stringify(productsContainer));
         displayProducts(productsContainer);
         clearForm();
-        
+
         updateButton.classList.replace('d-inline-block', 'd-none');
         addButton.classList.remove('d-none');
         currentUpdateIndex = -1;
+        window.alert('You updated your product.');
     }
-}  
-
+}
 
